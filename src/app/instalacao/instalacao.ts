73d1d0ffe6ba38887e63c0474763aa0942ad3bbd
Fernando Camilo <nando.f3@hotmail.com>
2025-10-29 20:16:15 -0300

    pwa

---
+++ src/app/instalacao/instalacao.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instalacao',
+  imports: [],
+  templateUrl: './instalacao.html',
+  styleUrl: './instalacao.scss',
+})
+export class Instalacao implements OnInit {
+
+  showInstallBanner = false;
+  deferredPrompt: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.showInstallBanner = true;
+    });
+  }
+
+  installApp() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        // if (choiceResult.outcome === 'accepted') {
+        //   console.log('Usuário aceitou instalar');
+        // } else {
+        //   console.log('Usuário recusou instalar');
+        // }
+        this.deferredPrompt = null;
+        this.showInstallBanner = false;
+      });
+    }
+  }
+
+  closeBanner(): void {
+    this.showInstallBanner = false;
+  }
+
+}
